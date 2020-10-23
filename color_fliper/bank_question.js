8736a21d0f27deb2293f95b9a95b755e945abe9f
Harun Yenial <harun.yenial@gmail.com>
2020-10-22 23:35:44 -0400

    Create bank_question.js

---
+++ color_fliper/bank_question.js
@@ -0,0 +1,25 @@
+// change the values of `balance`, `checkBalance`, and `isActive` to test your code
+var balance = -325;
+var checkBalance = true;
+var isActive = true;
+
+// Start only if checkBalance === true
+if (checkBalance === true) {
+    // Cases when account is active. Now, the balance could be <, =, or > zero
+    if (isActive === true && balance > 0) {
+        console.log("Your balance is $" + balance.toFixed(2) + ".");
+    }
+    else if (isActive === true && balance === 0){
+        console.log("Your account is empty.");
+    } 
+    else if (isActive === true && balance < 0){
+        console.log("Your balance is negative. Please contact bank.");
+    }
+    // Case when account is NOT active
+    else if (isActive === false) {
+        console.log("Your account is no longer active.");
+    }  
+} 
+else {
+  console.log("Thank you. Have a nice day!");
+}
