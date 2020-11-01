72f6038335bcb1eef00417f2c2c1cd2a01f9a7e2
Harun Yenial <harun.yenial@gmail.com>
2020-11-01 01:46:44 -0400

    Update question1.js

--- study_notes/question1.js
+++ study_notes/question1.js
@@ -22,3 +22,23 @@ console.log(divideByThree(result));
 //answer: 4 //
 
 
+
+// write a text which repeats 3 times of ha//
+//output should be hahaha! //
+
+// Declare an empty string
+var sound = "" ; 
+
+// An anonymous function expression stored in the variable `laugh`
+var laugh = function(num) {
+
+    //write loop
+    for (var x = 0 ; x < num ; x++) {
+        sound = sound + "ha" ;  
+    }
+    sound = sound +"!"; 
+    return sound; 
+}
+
+// It is essential that the function must return a string
+console.log(laugh(3)) ;
