ba9c5c2303c0e98cba6eae6282d9a7051a45c8ae
Harun Yenial <harun.yenial@gmail.com>
2020-11-01 00:52:09 -0400

    Create question1.js

---
+++ study_notes/question1.js
@@ -0,0 +1,24 @@
+
+//question 1//
+/*
+Try predicting what will be printed in the console.log statement below. 
+Then, check your prediction by pasting the code into the JavaScript console. 
+Functions can be tricky, so try figuring it out before running the code!
+*/
+
+
+function addTen(x) {
+  return x + 10;
+}
+
+function divideByThree(y) {
+  return y / 3;
+}
+
+var result = addTen(2);
+console.log(divideByThree(result));
+
+
+//answer: 4 //
+
+
