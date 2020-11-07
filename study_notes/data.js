6bc0a69737b5db98e7df0e2ed5100336903763e9
Harun Yenial <harun.yenial@gmail.com>
2020-11-06 23:59:35 -0500

    Update data.js

--- study_notes/data.js
+++ study_notes/data.js
@@ -50,3 +50,25 @@ serverRequest("The glass is half ", getResults);
 // Result in console after 5 second delay:
 // Response from the server: The glass is half full!
 
+
+function Person(firstName, lastName) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+}
+
+Person.prototype.log = function() {
+    console.log('I am', this._firstName, this._lastName);
+}
+
+
+//Using methods added to prototypes:
+
+// This line adds getters and setters for the profession object. Note that in general you could just write your own get and set functions like the 'log' method above.
+// Since in this example we are trying the mimic the class above, we try to use the getters and setters property provided by JavaScript
+Object.defineProperty(Person.prototype, 'profession', {
+    set: function(val) {
+        this._profession = val;
+    },
+    get: function() {
+        console.log(this._firstName, this._lastName, 'is a', this._profession);
+
