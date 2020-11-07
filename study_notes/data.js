f2d1c2184b3507d4e1c8078620fd904e8ab6f782
Harun Yenial <harun.yenial@gmail.com>
2020-11-06 23:58:40 -0500

    Update data.js

--- study_notes/data.js
+++ study_notes/data.js
@@ -32,3 +32,21 @@ createQuote("eat your vegetables!", logQuote); // 1
 
 // Result in console: 
 // Like I always say, eat your vegetables!
+
+
+function serverRequest(query, callback){
+  setTimeout(function(){
+    var response = query + "full!";
+    callback(response);
+  },5000);
+}
+
+function getResults(results){
+  console.log("Response from the server: " + results);
+}
+
+serverRequest("The glass is half ", getResults);
+
+// Result in console after 5 second delay:
+// Response from the server: The glass is half full!
+
