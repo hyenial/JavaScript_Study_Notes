ce54616100e347fe2aacb7e9518b29246f1c014e
Harun Yenial <harun.yenial@gmail.com>
2020-11-08 23:20:22 -0500

    Create nested_scope.js

---
+++ study_notes/nested_scope.js
@@ -0,0 +1,23 @@
+
+/*
+JavaScript distinguishes not just global and local bindings. Blocks and functions can be created inside other blocks and functions, producing multiple degrees of locality.
+
+For example, this function—which outputs the ingredients needed to make a batch of hummus—has another function inside it:
+
+*/
+
+const hummus = function(factor) {
+  const ingredient = function(amount, unit, name) {
+    let ingredientAmount = amount * factor;
+    if (ingredientAmount > 1) {
+      unit += "s";
+    }
+    console.log(`${ingredientAmount} ${unit} ${name}`);
+  };
+  ingredient(1, "can", "chickpeas");
+  ingredient(0.25, "cup", "tahini");
+  ingredient(0.25, "cup", "lemon juice");
+  ingredient(1, "clove", "garlic");
+  ingredient(2, "tablespoon", "olive oil");
+  ingredient(0.5, "teaspoon", "cumin");
+};
