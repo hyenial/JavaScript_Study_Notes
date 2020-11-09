03a728d546627a14eabad516969c85d3a02f9bc1
Harun Yenial <harun.yenial@gmail.com>
2020-11-08 23:20:47 -0500

    Update nested_scope.js

--- study_notes/nested_scope.js
+++ study_notes/nested_scope.js
@@ -1,7 +1,7 @@
 
 /*
-JavaScript distinguishes not just global and local bindings. Blocks and functions can be created inside other blocks and functions, producing multiple degrees of locality.
-
+JavaScript distinguishes not just global and local bindings. Blocks and functions can be created inside other blocks and functions, 
+producing multiple degrees of locality.
 For example, this function—which outputs the ingredients needed to make a batch of hummus—has another function inside it:
 
 */
