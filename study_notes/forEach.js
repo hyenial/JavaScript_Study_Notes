d610e1d40c5bfa12fc24d53c0dad7f13d8e08594
Harun Yenial <harun.yenial@gmail.com>
2020-11-18 00:27:23 -0500

    Update forEach.js

--- study_notes/forEach.js
+++ study_notes/forEach.js
@@ -2,3 +2,38 @@ words = ["cat", "in", "hat"];
 words.forEach(function(word, num, all) {
   console.log("Word " + num + " in " + all.toString() + " is " + word);
 });
+
+/*
+ * Programming Quiz: Another Type of Loop (6-8)
+ * QUIZ REQUIREMENTS
+ * Use the existing `test` variable and write a `forEach` loop
+ * that adds 100 to each number that is divisible by 3.
+ *
+ * Things to note:
+ *  - Inside the loop, you must use an `if` statement to verify code is divisible by 3
+ *  - Inside the loop, you can updade an element ONLY by using the arrayName[index]
+ *  - Outside the loop, you can use `console.log` to verify the `test` variable 
+ */
+
+
+
+
+
+
+var test = [12, 929, 11, 3, 199, 1000, 7, 1, 24, 37, 4,
+    19, 300, 3775, 299, 36, 209, 148, 169, 299,
+    6, 109, 20, 58, 139, 59, 3, 1, 139
+];
+
+test.forEach(function(item, index){
+    if(item%3===0){
+        // The commented statements below would not be able to change the value of item
+        // because the `item` is a COPY of actual element, while actual is test[index]
+        // item = item + 100;
+        // console.log("The updated element is "+item+" and actual is "+test[index]);
+        test[index]=test[index]+100;
+    }
+    //else 
+    //    console.log("Element "+item+" is not divisible by 3");
+});
+console.log(test);
