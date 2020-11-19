b0be5ae5328a74ad27ca2088334f99c4eefd9285
Harun Yenial <harun.yenial@gmail.com>
2020-11-18 21:18:39 -0500

    Create facebook.js

---
+++ study_notes/facebook.js
@@ -0,0 +1,46 @@
+/*
+ * Programming Quiz: Facebook Friends (7-5)
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have an object `facebookProfile`
+ * - The `facebookProfile` object should have the `name` (string), `friends` (number), and `messages` (array of strings) property
+ * - Your `facebookProfile` object should have the `postMessage()`, `deleteMessage()`, `addFriend()` and `removeFriend()` method
+ * - Carefully implement the desired functionality of each method, as decribed above
+ */
+
+
+// TIP - 
+// In an array, 
+// - addition at the end is done using push() method
+// - addition at a specific index is done using splice() method
+// - deletion from the beginning is done using pop() method
+// - deletion from a specific index is done using splice() method
+
+// your code goes here
+
+
+
+var facebookProfile = {
+    name: "harun",
+    friends: 25,
+    messages: ["Message 1", "Message 2", "Message 3", "Message 4"],
+    postMessage: function(message){
+        facebookProfile.messages.push(message);
+    },
+    deleteMessage: function(index){
+        // In the following splice() method call,
+        // - argument 1 = index from where the element has to be deleted
+        // - argument 2 = count of elements to be deleted
+        facebookProfile.messages.splice(index, 1);
+    },
+    addFriend: function(){
+        facebookProfile.friends = facebookProfile.friends + 1;
+        //friends += 1; // this statement is equivalent to the above
+    },
+    removeFriend: function(){
+        if(facebookProfile.friends>0)
+            facebookProfile.friends = facebookProfile.friends - 1;
+    }
+};
