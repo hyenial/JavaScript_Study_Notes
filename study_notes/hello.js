7eab5f093bc2eaa32e97dfd520a31f1845177f4a
Harun Yenial <harun.yenial@gmail.com>
2020-11-22 23:40:52 -0500

    Create hello.js

---
+++ study_notes/hello.js
@@ -0,0 +1,8 @@
+function greet(message) {
+console.log(message);
+}
+function greeter(name, age) {
+return name + " says Hey!! He is " + age + " years old";
+}
+var message = greeter("harun", 38);
+greet(message);
