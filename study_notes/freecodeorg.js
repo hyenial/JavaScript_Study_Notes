7b7ecced415ccd871a020cfc1a1e6744a7f770b2
Harun Yenial <harun.yenial@gmail.com>
2020-12-21 19:30:31 -0500

    Update freecodeorg.js

--- study_notes/freecodeorg.js
+++ study_notes/freecodeorg.js
@@ -2,7 +2,7 @@
 Truncate a string (first argument) if it is longer than the given maximum string length (second argument). 
 Return the truncated string with a ... ending.
 
-*\
+*/
 
 function truncateString(str, num) {
   if (str.length > num) {
@@ -13,3 +13,23 @@ function truncateString(str, num) {
 }
 
 truncateString("A-tisket a-tasket A green and yellow basket", 8);
+
+
+/*
+Create a function that looks through an array arr and returns the first element in it that passes a 'truth test'. 
+This means that given an element x, the 'truth test' is passed if func(x) is true. 
+If no element passes the test, return undefined.
+*/
+
+function findElement(arr, func) {
+  let num = 0;
+
+  for (var i = 0; i < arr.length; i++) {
+    num = arr[i];
+    if (func(num)) {
+      return num;
+    }
+  }
+
+  return undefined;
+}
