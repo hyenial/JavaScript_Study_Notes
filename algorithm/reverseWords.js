080f270d02ff3424ed6ad4ef710b60a41721cb58
Harun Yenial <harun.yenial@gmail.com>
2020-12-22 19:44:25 -0500

    Update reverseWords.js

--- algorithm/reverseWords.js
+++ algorithm/reverseWords.js
@@ -1,1 +1,19 @@
+/*
+function reverseWords(string) {
+  var wordsArr = string.split(' ');
+  var reversedWordsArr = [];
+  
+  wordsArr.forEach(word => {
+    var reversedWord = '';
+    for (var i = word.length - 1; i >= 0; i--) {
+      reversedWord += word[i];
+    };
+    reversedWordsArr.push(reversedWord);
+  });
+  
+  return reversedWordsArr.join(' ');
+}
+ 
+reverseWords('Coding JavaScript');
 
+*/
