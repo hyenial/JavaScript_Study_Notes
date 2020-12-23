19f0f36e4aba5348cbef877ee49fe6a2bff256ec
Harun Yenial <harun.yenial@gmail.com>
2020-12-22 19:32:19 -0500

    Update caesarChipher.js

--- algorithm/caesarChipher.js
+++ algorithm/caesarChipher.js
@@ -11,3 +11,28 @@ As with all single-alphabet substitution ciphers, the Caesar cipher is easily br
 referance : https://en.wikipedia.org/wiki/Caesar_cipher
 */
 
+function caesarCipher(str,num) {
+  num = num % 26;
+  var lowerCaseString = str.toLowerCase();
+  var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  var newString = '';
+  
+  for (var i = 0; i < lowerCaseString.length; i++) {
+    var currentLetter = lowerCaseString[i];
+    if (currentLetter === ' ') {
+      newString += currentLetter;
+      continue;
+    }
+    var currentIndex = alphabet.indexOf(currentLetter);
+    var newIndex = currentIndex + num;
+    if (newIndex > 25) newIndex = newIndex - 26;
+    if (newIndex < 0) newIndex = 26 + newIndex;
+    if (str[i] === str[i].toUpperCase()) {
+      newString += alphabet[newIndex].toUpperCase();
+    }
+    else newString += alphabet[newIndex];
+  };
+  
+  return newString;
+}
+caesarCipher('write your text', 2); // test your code
