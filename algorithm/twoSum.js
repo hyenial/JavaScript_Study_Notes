606a16da2121f87eb88b60354084c99f1dc42c48
Harun Yenial <harun.yenial@gmail.com>
2020-12-22 21:59:34 -0500

    Create twoSum.js

---
+++ algorithm/twoSum.js
@@ -0,0 +1,21 @@
+/*
+function twoSum(numArray, sum) {
+  var pairs = [];
+  var hashTable = [];
+ 
+  for (var i = 0; i < numArray.length; i++) {
+    var currNum = numArray[i];
+    var counterpart = sum - currNum;
+    if (hashTable.indexOf(counterpart) !== -1) {
+      pairs.push([ currNum, counterpart ]);
+    }
+    hashTable.push(currNum);
+  }
+  
+  return pairs;
+}
+ 
+twoSum([1, 6, 4, 5, 3, 3], 7);
+
+
+*/
