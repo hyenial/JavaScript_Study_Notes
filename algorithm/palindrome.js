8b40a16066da4d86f53b3274902b32d44f11208b
Harun Yenial <harun.yenial@gmail.com>
2020-12-22 19:07:36 -0500

    Create palindrome.js

---
+++ algorithm/palindrome.js
@@ -0,0 +1,18 @@
+// this function returns if text is palindrome
+
+function isPalindrome(string) {
+  string = string.toLowerCase();
+  var charactersArr = string.split('');
+  var validCharacters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  
+  var lettersArr = [];
+  charactersArr.forEach(char => {
+    if (validCharacters.indexOf(char) > -1) lettersArr.push(char);
+  });
+  
+  return lettersArr.join('') === lettersArr.reverse().join('');
+}
+ 
+isPalindrome("write ant text here"); // you can test here
+
+//console.log(isPalindrome("write ant text here"));
