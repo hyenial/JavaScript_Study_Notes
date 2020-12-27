86be370f5b0cf9f1aa66fe4d1424d891955dc7ec
Harun Yenial <harun.yenial@gmail.com>
2020-12-26 23:43:22 -0500

    Update bianarysearch.js

--- algorithm/bianarysearch.js
+++ algorithm/bianarysearch.js
@@ -1,13 +1,16 @@
+// bianary search is looking for the element according to half of the index...
 function binarySearch(numArray, key) {
     var middleIdx = Math.floor(numArray.length / 2);
     var middleElem = numArray[middleIdx];
     
     if (middleElem === key) return true;
     else if (middleElem < key && numArray.length > 1) {
-        return binarySearch(numArray.splice(middleIdx, numArray.length), key);
+        return binarySearch(numArray.splice(middleIdx, numArray.length), key); 
+        // splice method here strarting point and ending point. it takes secand half of the function
     }
     else if (middleElem > key && numArray.length > 1) {
         return binarySearch(numArray.splice(0, middleIdx), key);
+        // it takes first half of the fuction
     }
     else return false;
 }
