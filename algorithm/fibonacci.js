62bf004d3a284821463023adec4078a22f6eff71
Harun Yenial <harun.yenial@gmail.com>
2020-12-27 01:52:53 -0500

    Update and rename fibonacci to fibonacci.js

---
+++ algorithm/fibonacci.js
@@ -0,0 +1,25 @@
+// creates fibonacci function
+function fibonacci(position) {
+  if (position < 3) return 1;
+  else return fibonacci(position - 1) + fibonacci(position - 2);
+}
+ 
+fibonacci(position);
+//
+
+// this function has high timecomlexity. this is not recomended....
+
+
+function fibMemo(index, cache) {
+  cache = cache || [];
+  if (cache[index]) return cache[index];
+  else {
+    if (index < 3) return 1;
+    else {
+      cache[index] = fibMemo(index - 1, cache) + fibMemo(index - 2, cache);
+    }
+  }
+  return cache[index];
+}
+ 
+fibMemo(500);
