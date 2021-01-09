3dd60d24b1dd6d5563b174c7094a2c6353693846
Harun Yenial <harun.yenial@gmail.com>
2021-01-08 23:24:51 -0500

    Update closure.js

--- objects/closure.js
+++ objects/closure.js
@@ -14,3 +14,29 @@ const result = myFunction();
 
 result();
 // 1
+
+
+// another example
+
+function myCounter() {
+  let count = 0;
+
+  return function () {
+    count += 1;
+    return count;
+  };
+}
+
+let counter = myCounter();
+
+counter();
+// 1
+
+counter();
+// 2
+
+counter.count;
+// undefined
+
+count;
+// undefined
