ae2960ddf431b18bada67d19cae9ab810ffab041
Harun Yenial <harun.yenial@gmail.com>
2021-01-11 23:06:32 -0500

    Create apply.js

---
+++ objects/apply.js
@@ -0,0 +1,12 @@
+
+const cat = {
+  name: 'Bailey'
+};
+
+function sayHello(message) {
+  console.log(`${message}, ${this.name}`);
+}
+
+sayHello.call(cat, 'Nice to see you');
+
+sayHello.apply(cat, ['Hello']);
