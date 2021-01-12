547dd47d93ed02eb25db964a7d860fe6d03e786b
Harun Yenial <harun.yenial@gmail.com>
2021-01-11 23:18:29 -0500

    Update call.js

--- objects/call.js
+++ objects/call.js
@@ -15,3 +15,23 @@ const pride = {
 
 mockingbird.describe.call(pride);
 // 'Pride and Prejudice is a classic novel'
+
+
+
+ 
+function invokeTwice(cb) {
+   cb();
+   cb();
+}
+
+const dog = {
+  age: 5,
+  growOneYear: function () {
+    this.age += 1;
+  }
+};
+
+const myGrow = dog.growOneYear.bind(dog);
+
+dog.age;
+// 7
