35b5169e8ec703858b38c571c4f350ecde5efc1b
Harun Yenial <harun.yenial@gmail.com>
2021-01-24 22:31:36 -0500

    Update freemomory.js

--- nodeJS/freemomory.js
+++ nodeJS/freemomory.js
@@ -1,1 +1,11 @@
 // import os library to the node environment
+
+
+const os = require('os');
+var totalMemory = os.totalmem()
+var freeMomory = os.freemem();
+
+//console.log('total memory '+ totalMemory); this also works
+
+console.log(`Total memory: ${totalMemory}`); //this is ES6
+console.log(`Free memory: ${freeMomory}`); //this is ES6
