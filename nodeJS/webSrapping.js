2f7492408f58ce62a134faffad63f12d066ff121
Harun Yenial <harun.yenial@gmail.com>
2021-01-28 23:40:50 -0500

    update: web link

--- nodeJS/webSrapping.js
+++ nodeJS/webSrapping.js
@@ -8,7 +8,7 @@ const writeStream = fs.createWriteStream('post.csv');
 // Write Headers
 writeStream.write(`Title,Link,Date \n`);
 
-request('http://codedemos.com/sampleblog', (error, response, html) => {
+request('http://54.90.149.89/', (error, response, html) => {
   if (!error && response.statusCode == 200) {
     const $ = cheerio.load(html);
 
