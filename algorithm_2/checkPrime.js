ecc4c4aac68bb46307e14b0ff2ee6e662028a893
Harun Yenial <harun.yenial@gmail.com>
2021-07-22 03:37:33 -0400

    Create checkPrime.js

---
+++ algorithm_2/checkPrime.js
@@ -0,0 +1,22 @@
+/*
+Question: How would you verify a prime number?
+
+Answer: a prime number is only divisible by itself and 1. So, i will run a while loop and increase by 1. 
+(look at the code example. If you dont get it. this is not your cake. do learn javaScript basics and come back.)
+
+*/
+
+function isPrime(n){
+  var divisor = 2;
+
+  while (n > divisor){
+    if(n % divisor == 0){
+     return false; 
+    }
+    else
+      divisor++;
+  }
+  return true;
+}
+
+# isPrime(n);
