0e63dba16061a09a6c603a8c14dd43d6d97d36d7
Harun Yenial <harun.yenial@gmail.com>
2021-08-01 21:02:33 -0400

    Create removeDuplicate.js

---
+++ algorithm_2/removeDuplicate.js
@@ -0,0 +1,18 @@
+
+function removeDuplicate(arr){
+  var exists ={},
+      outArr = [], 
+      elm;
+
+  for(var i =0; i<arr.length; i++){
+    elm = arr[i];
+    if(!exists[elm]){
+      outArr.push(elm);
+      exists[elm] = true;
+   }
+  }
+  return outArr;
+}
+
+> removeDuplicate([1,3,3,3,1,5,6,7,8,1]);
+  = [1, 3, 5, 6, 7, 8]
