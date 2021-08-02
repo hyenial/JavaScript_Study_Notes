575b3592cabbf640f571f954e473922a42d08c0b
Harun Yenial <harun.yenial@gmail.com>
2021-08-01 21:03:22 -0400

    Update removeDuplicate.js

--- algorithm_2/removeDuplicate.js
+++ algorithm_2/removeDuplicate.js
@@ -1,3 +1,11 @@
+/*
+Question: How would you remove duplicate members from an array?
+
+Answer: will start a while looping and keep an object/ associated array. 
+If i find an element for the first time i will set its value as true (that will tell me element added once.). 
+if i find a element in the exists object, i will not insert it into the return array.
+
+*/
 
 function removeDuplicate(arr){
   var exists ={},
